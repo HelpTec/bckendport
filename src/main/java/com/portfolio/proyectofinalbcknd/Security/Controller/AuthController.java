d0d01a2313399607d6e965bed0223a5bb4a14acb
HelpTec <diegoamezaga@gmail.com>
2022-08-23 22:32:49 -0300

    prueba

--- src/main/java/com/portfolio/proyectofinalbcknd/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/proyectofinalbcknd/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://hosting-angular-portfolio.web.app")
+@CrossOrigin(origins = "")
 public class AuthController {
     @Autowired PasswordEncoder passwordEncoder;
     @Autowired AuthenticationManager authManager;
